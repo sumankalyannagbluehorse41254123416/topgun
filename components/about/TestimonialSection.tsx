0277544c96d44112612d5d2cbe52bd812f21838b
Dev_Srijani <srijani.bluehorse@mail.com>
2025-10-27 11:31:28 +0530

    changes

--- components/about/TestimonialSection.tsx
+++ components/about/TestimonialSection.tsx
@@ -32,52 +32,54 @@ export default function TestimonialSection() {
   ];
 
   return (
-    <div className="container">
-      <div className="section-head text-center text-white">
-        <h3 className="h3">TESTIMONIAL</h3>
-        <div className="dez-separator bg-primary"></div>
-        <p>
-          Our academy is a factory for producing Champions. Every year new
-          people join us and we help them reach their potential.
-        </p>
-      </div>
+    <div className="section-full bg-img-fix content-inner overlay-black-dark">
+      <div className="container">
+        <div className="section-head text-center text-white">
+          <h3 className="h3">TESTIMONIAL</h3>
+          <div className="dez-separator bg-primary"></div>
+          <p>
+            Our academy is a factory for producing Champions. Every year new
+            people join us and we help them reach their potential.
+          </p>
+        </div>
 
-      <div className="section-content">
-        <Swiper
-          modules={[Autoplay, Pagination]}
-          slidesPerView={1}
-          spaceBetween={30}
-          loop
-          autoplay={{ delay: 4000 }}
-          pagination={{ clickable: true }}
-          breakpoints={{
-            1024: { slidesPerView: 2 },
-          }}
-          className="testimonial-five"
-        >
-          {testimonials.map((t, i) => (
-            <SwiperSlide key={i}>
-              <div className="testimonial-6">
-                <div className="testimonial-text bg-white quote-left quote-right">
-                  <p>{t.text}</p>
-                </div>
-                <div className="testimonial-detail clearfix bg-primary text-white">
-                  <h4 className="testimonial-name m-tb0">{t.name}</h4>
-                  <span className="testimonial-position">{t.position}</span>
-                  <div className="testimonial-pic radius">
-                    <Image
-                      src={t.image}
-                      alt={t.text}
-                      width={100}
-                      height={100}
-                      className="rounded-full"
-                    />
+        <div className="section-content">
+          <Swiper
+            modules={[Autoplay, Pagination]}
+            slidesPerView={1}
+            spaceBetween={30}
+            loop
+            autoplay={{ delay: 4000 }}
+            pagination={{ clickable: true }}
+            breakpoints={{
+              1024: { slidesPerView: 2 },
+            }}
+            className="testimonial-five"
+          >
+            {testimonials.map((t, i) => (
+              <SwiperSlide key={i}>
+                <div className="testimonial-6">
+                  <div className="testimonial-text bg-white quote-left quote-right">
+                    <p>{t.text}</p>
+                  </div>
+                  <div className="testimonial-detail clearfix bg-primary text-white">
+                    <h4 className="testimonial-name m-tb0">{t.name}</h4>
+                    <span className="testimonial-position">{t.position}</span>
+                    <div className="testimonial-pic radius">
+                      <Image
+                        src={t.image}
+                        alt={t.text}
+                        width={100}
+                        height={100}
+                        className="rounded-full"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </div>
   );
